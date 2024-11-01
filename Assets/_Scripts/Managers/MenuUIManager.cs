c7c6528283b4b00d330f6aadb8e4795c4dfeb3b7
Stealthness <stevewestinlondon@gmail.com>
2024-11-01 23:33:07 +0000

    MenuUIManager complete

--- Assets/_Scripts/Managers/MenuUIManager.cs
+++ Assets/_Scripts/Managers/MenuUIManager.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace _Scripts.Managers
 {
     public class MenuUIManager: MonoBehaviour
     {
-        
+        public void StartGame()
+        {
+            SceneManager.LoadScene("BeachScene");
+        }
     }
 }
