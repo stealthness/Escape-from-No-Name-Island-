eb21d85904c6a9891b240cd65a6b9ca78acbacd5
Stealthness <stevewestinlondon@gmail.com>
2024-11-02 00:08:03 +0000

    add dun and moon light

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -4,12 +4,26 @@ namespace _Scripts
 {
     public class GameManager : MonoBehaviour
     {
+
+        public GameObject MoonLight;
+        public GameObject SunLight;
+        
+        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             Debug.Log("Game Started");
+            
+            InvokeRepeating(nameof(ChangePassiveLight), 10, 20);
         }
 
+        public void ChangePassiveLight()
+        {
+            Debug.Log("ChangePassiveLight");
+            MoonLight.SetActive(!MoonLight.activeSelf);
+            SunLight.SetActive(!SunLight.activeSelf);
+        }
+        
         public void GameOver()
         {
             Time.timeScale = 0;
